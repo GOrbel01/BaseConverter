f78cb6c643d1400b6f8e0ed5685617b669588faa
George Orbell <gorbel01@mail.bbk.ac.uk>
2017-10-11 11:37:52 +0100

    Implement base validator classes

---
+++ src/main/java/com/geo/decconv/event/validation/validator/DecimalValidator.java
@@ -0,0 +1,14 @@
+package com.geo.decconv.event.validation.validator;
+
+import com.geo.decconv.event.validation.ValidationResult;
+
+/**
+ * Created by Tidus on 11/10/2017.
+ */
+public class DecimalValidator implements Validator {
+    @Override
+    public ValidationResult validateData(String text) {
+        //TODO Implement.
+        return null;
+    }
+}
---
+++ src/main/java/com/geo/decconv/event/validation/validator/HexValidator.java
@@ -0,0 +1,14 @@
+package com.geo.decconv.event.validation.validator;
+
+import com.geo.decconv.event.validation.ValidationResult;
+
+/**
+ * Created by Tidus on 11/10/2017.
+ */
+public class HexValidator implements Validator {
+    @Override
+    public ValidationResult validateData(String text) {
+        //TODO Implement.
+        return null;
+    }
+}
