cbd09f820e07e166916a4b4aa34f6e51cd865e47
George Orbell <F1n4lCloud@googlemail.com>
2017-10-11 11:57:24 +0100

    Written HexString tests.

--- src/test/java/com/validation/test/TestHexStringValidation.java
+++ src/test/java/com/validation/test/TestHexStringValidation.java
@@ -1,6 +1,10 @@
 package com.validation.test;
 
+import com.geo.decconv.event.validation.validator.HexValidator;
+import com.geo.decconv.event.validation.validator.Validator;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Tidus on 11/10/2017.
@@ -8,6 +12,18 @@ import org.junit.Test;
 public class TestHexStringValidation {
     @Test
     public void testHexStringValidation() {
+        Validator validator = new HexValidator();
 
+        String test = "4012A";
+        assertEquals(true, validator.validateData(test).isValid());
+
+        test = "65A44G";
+        assertEquals(false, validator.validateData(test).isValid());
+
+        test = "GHCHZSFJL";
+        assertEquals(false, validator.validateData(test).isValid());
+
+        test = "8A6S";
+        assertEquals(false, validator.validateData(test).isValid());
     }
 }
