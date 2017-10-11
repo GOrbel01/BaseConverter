58164785fdf4e1e1e8de3e0e3f3f215d513bd9dd
George Orbell <F1n4lCloud@googlemail.com>
2017-10-11 11:41:59 +0100

    Added test classes for hex and decimal string validation

---
+++ src/test/java/com/validation/test/TestDecimalStringValidation.java
@@ -0,0 +1,13 @@
+package com.validation.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Tidus on 11/10/2017.
+ */
+public class TestDecimalStringValidation {
+    @Test
+    public void testDecimalStringValidation() {
+
+    }
+}
---
+++ src/test/java/com/validation/test/TestHexStringValidation.java
@@ -0,0 +1,13 @@
+package com.validation.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Tidus on 11/10/2017.
+ */
+public class TestHexStringValidation {
+    @Test
+    public void testHexStringValidation() {
+
+    }
+}
